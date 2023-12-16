da5f4b9e58c8170e6bef18434974e7d39db3c4d3
Patrizio Milione <patriziomilione@gmail.com>
2023-12-16 11:34:29 +0100

    daje

--- src/components/views/InSelf.vue
+++ src/components/views/InSelf.vue
@@ -182,3 +182,267 @@
 <script>
 export default {}
 </script>
+
+<style scoped>
+.onshelf {
+  width: 100%;
+  text-align: center;
+}
+
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 90vh;
+  width: 100%;
+  background: url('/images/6132_60.png') no-repeat center center / cover;
+}
+
+.header h1 {
+  font-size: 200px;
+  color: var(--four);
+  font-family: 'Covered By Your Grace';
+  text-shadow: 10px 0 10px var(--two);
+}
+
+.header h2 {
+  font-size: 40px;
+  color: var(--four);
+  text-shadow: 10px 0 10px var(--two);
+}
+
+.sub_header {
+  height: 40vh;
+  text-align: center;
+  font-size: 25px;
+  padding: 1rem 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  box-shadow: rgba(0, 0, 0, 0.56) 0px 3px 15px 1px inset;
+}
+
+.sub_header_bottom {
+  height: 40vh;
+  text-align: center;
+  font-size: 40px;
+  padding: 1rem 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  box-shadow: rgba(0, 0, 0, 0.56) 0px 3px 15px 1px inset;
+}
+
+.sub_header p {
+  line-height: 1.4;
+}
+
+h1 {
+  font-size: 50px;
+  color: var(--white);
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.content_header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 90vh;
+  position: relative;
+}
+
+.content_header_title {
+  font-size: 40px;
+  color: var(--one);
+  position: absolute;
+  z-index: 100;
+  top: 5%;
+  left: 5%;
+}
+
+.content .image-section {
+  order: 2;
+}
+
+.image-section,
+.text-section {
+  flex: 1;
+}
+
+.image-section img {
+  height: 85vh;
+  border-radius: 15px;
+  box-shadow: 3px 4px 8px var(--one);
+}
+
+.text-section h2,
+.text-section h1 {
+  font-size: 2em;
+  color: #000;
+  margin-bottom: 0.5em;
+  font-family: 'Roboto Slab', serif;
+}
+
+.text-section h1 {
+  font-size: 40px;
+  display: none;
+}
+
+.text-section p {
+  font-size: 23px;
+  line-height: 1.4;
+  color: #212121;
+  padding: 0 4rem;
+  text-align: left;
+}
+
+.service_section {
+  text-align: center;
+  padding: 4rem;
+  background-color: var(--one);
+}
+
+.service_header {
+  font-size: 2em;
+  margin-bottom: 0.5em;
+  color: var(--two);
+}
+
+.service_subheader {
+  font-size: 1.5em;
+  margin-bottom: 2em;
+  color: var(--two);
+}
+
+.services {
+  display: flex;
+  justify-content: space-between;
+}
+
+.service {
+  width: 30%;
+  padding: 2rem;
+  box-shadow: 3px 4px 8px var(--two);
+  margin: 1rem;
+  background: var(--white);
+  border-radius: 5px;
+}
+
+.service h3 {
+  font-size: 1.5em;
+  color: var(--one);
+  margin-bottom: 1.5em;
+}
+
+.service p {
+  font-size: 1em;
+  line-height: 1.3;
+  color: var(--text-color);
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  flex-direction: column;
+}
+
+.results_section {
+  padding: 4rem;
+  background: var(--two);
+}
+
+.results_header {
+  font-size: 2.5em;
+  margin-bottom: 1em;
+  color: var(--one);
+}
+
+.results {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.result {
+  width: 45%;
+  padding: 2rem;
+  margin: 1rem;
+  background: var(--white);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  height: 28vh;
+}
+
+.result h3 {
+  font-size: 1.4em;
+  color: var(--one);
+  margin-bottom: 2rem;
+  height: 30%;
+}
+
+.result p {
+  font-size: 1em;
+  line-height: 1.3;
+  color: var(--text-color);
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  flex-direction: column;
+}
+
+.collaboration_section {
+  padding: 4rem;
+  text-align: center;
+}
+
+.collaboration_header {
+  font-size: 2em;
+  margin-bottom: 1em;
+  text-align: center;
+}
+
+.collaboration_list {
+  list-style-type: none; /* Remove default bullets */
+  padding: 0;
+  margin: 0;
+}
+
+.collaboration_list li {
+  background: url('/path/to/bullet-image.svg') no-repeat left center; /* Replace with your bullet image path */
+  padding-left: 40px; /* Adjust depending on your bullet image size */
+  margin-bottom: 1rem; /* Space between list items */
+  text-align: center;
+  font-size: 1em;
+  line-height: 1.6;
+}
+
+.list_title {
+  font-weight: bold;
+}
+
+.btn_bottom {
+  font-size: 30px;
+  padding: 1rem;
+  box-shadow: 3px 4px 8px var(--one);
+}
+
+.btn_bottom:hover {
+  background-color: var(--two);
+  box-shadow: 3px 4px 8px var(--two);
+}
+
+.btn_bottom a {
+  color: var(--two);
+}
+.btn_bottom a:hover {
+  color: var(--one);
+}
+</style>
